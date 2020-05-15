bacee7cb705ad0ebf7415bdda819119ca72c18d1
Raotrux <56573919+Raotrux@users.noreply.github.com>
2020-05-15 11:50:06 -0700

    Week 5 Push 1

--- app/src/main/java/com/example/ad340app/MainActivity.kt
+++ app/src/main/java/com/example/ad340app/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.ad340app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
@@ -11,7 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ad340app.details.ForecastDetailsActivity
-import java.util.*
+import com.example.ad340app.location.LocationEntryFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,26 +30,41 @@ class MainActivity : AppCompatActivity() {
         val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
 
         submitButton.setOnClickListener {
-            val zipcode = zipcodeEditText.text.toString()
+            val zipcode: String = zipcodeEditText.text.toString()
+
             if (zipcode.length != 5) {
                 Toast.makeText(this, R.string.error_output, Toast.LENGTH_SHORT).show()
             } else {
                 forecastRepository.loadForecast(zipcode)
             }
-
-            val forecastList: RecyclerView = findViewById(R.id.forecastList)
-
-            forecastList.layoutManager = LinearLayoutManager(this)
-            val dailyForecastAdapter = DailyForecastAdapter(tempDisplaySettingManager) {forecastItem ->
-                showForecastDetails(forecastItem)
-            }
-            forecastList.adapter = dailyForecastAdapter
-
-            val weeklyForecastObserver = Observer<List<DailyForecast>> { forecastItems ->
-                dailyForecastAdapter.submitList(forecastItems)
-            }
-            forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
         }
+
+
+        val dailyForecastList: RecyclerView = findViewById(R.id.dailyForecastList)
+
+        dailyForecastList.layoutManager = LinearLayoutManager(this)
+        val dailyForecastAdapter = DailyForecastAdapter(tempDisplaySettingManager) {
+            showForecastDetails(it)
+        }
+        dailyForecastList.adapter = dailyForecastAdapter
+
+
+        val weeklyForecastObserver = Observer<List<DailyForecast>> { forecastItems ->
+            dailyForecastAdapter.submitList(forecastItems)
+        }
+
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainer, LocationEntryFragment())
+            .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true;
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
